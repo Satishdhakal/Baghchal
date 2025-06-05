d587d699318d6e3e52efe3836c15c5d3614f32cf
sandesh dhakal <sdhakal712@gmail.com>
2025-06-05 20:07:57 +0545

    placing pieces and moving prototype

--- models.py
+++ models.py
@@ -36,11 +36,63 @@ class Gameboard: # gamestate
         self.sheep_position = []
 
         for i in self.tiger_position:
-            pass
+            self._place_pieces(i,PieceType.TIGER)
+        
 
 
     def _init_board(self) -> dict:
         #generate board 5X5 for positioning ease
         return {(i,j):PieceType.EMPTY for i in range(5) for j in range(5)}
+    
+    def _place_pieces(self,position : Tuple[int,int],pieces : PieceType):
+        if self.board[position] != PieceType.EMPTY:
+            raise ValueError('Position already Occupied')
+        self.board[position] = pieces
+        if pieces == PieceType.SHEEP:
+            self.sheep_position.append(position)
+
+    def move_sheep(self,position : Tuple[int,int],destination : Tuple[int,int],piece : PieceType):
+        if piece != PieceType.SHEEP:
+            raise ValueError('You can only move sheeps')
+        if self.board[position] != PieceType.SHEEP:
+            raise ValueError('Select a sheep to move')
+        if destination not in VALID_POSITIONS.get(position,set()): #set lookup with a safe landing
+            raise ValueError('Invalid Move')
+        if self.board[destination] != PieceType.EMPTY:
+            raise ValueError('Invalid Move')
+        self.board[position] = destination
+        self.board[position] = PieceType.EMPTY
+        self.board[destination] = PieceType.SHEEP
+        print(VALID_POSITIONS[position])
+        print(VALID_POSITIONS[destination])
+
+
+
+
+
+
+        
+    
+    def display_board(self):
+        for i in range(5):
+            row = ''
+            for j in range(5):
+                piece = self.board[(i, j)]
+                if piece == PieceType.EMPTY:
+                    row += '. '
+                elif piece == PieceType.TIGER:
+                    row += 'T '
+                else:
+                    row += 'S '
+            print(row)
+        print()
+
+
+g = Gameboard()
+g._place_pieces((1,3),PieceType.SHEEP)
+
+g.move_sheep((1,3),(1,2),PieceType.SHEEP)
+g.display_board()
+
     
 
